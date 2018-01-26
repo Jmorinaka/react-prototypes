0706e9745b75afd825f40b7d8f84dd604dcac3d0
Justin Morinaka <justin_morinaka@yahoo.com>
2018-01-26 11:38:21 -0800

    finished prototype

---
+++ src/components/format_time.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class FormatTime extends Component {
+    constructor(props){
+        super(props);
+
+        this.timerStyle ={
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '295px'
+        };
+
+        this.state = {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            ms: 0
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        const {elapsed} = nextProps;
+        const sec = elapsed / 1000;
+        const min = sec / 60;
+        this.setState({
+            hour: Math.floor(min / 60),
+            min: Math.floor(min % 60),
+            sec: Math.floor(sec % 60),
+            ms: Math.floor(elapsed % 100)
+        })
+
+    }
+
+
+    render(){
+        const {hour,min,sec,ms} = this.state;
+        return (
+            <div style={this.timerStlye}>{hour}:{min}:{sec}.{ms}</div>
+        )
+    }
+}
+
+export default FormatTime;
